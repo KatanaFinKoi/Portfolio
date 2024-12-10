af46b15e4caee6e8daab8dbf862f1da59ebf2b90
Liam Rayback <liam@macbook.local>
2024-12-10 16:04:35 -0700

    updates to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Navbar from './components/Navbar'
-import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+// import Navbar from './components/Navbar'
+import { Routes, Route, } from 'react-router-dom'
 import AboutMe from './Pages/aboutMe'
 import Contact from './Pages/contact'
 import Portfolio from './Pages/portfolio'
@@ -11,17 +11,17 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Header />
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/aboutMe" element={<AboutMe />} />
+        <Route path="/" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </div>
   )
 };
 
