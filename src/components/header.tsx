60d0de4b613299b9b21f2a4260951efc1f575371
Liam Rayback <liam@macbook.local>
2024-12-13 08:28:26 -0700

    header now works as intended

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,23 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-// const navigate = useNavigate();
+
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+  <div>
+    <h1>
+      Liam Rayback
+    </h1>
+    <div>
+      <button onClick={() => navigate("/aboutMe")}>About Me</button>
+      <button onClick={() => navigate("/portfolio")}>Portfolio</button>
+      <button onClick={() => navigate("/contact")}>Contact</button>
+      <button onClick={() => navigate("/resume")}>Resume</button>
+    </div>
+  </div>
+  );
+}
 
 // const Header = () => {
 //   return (
@@ -8,30 +25,14 @@
 //     <h1>
 //       Liam Rayback
 //     </h1>
-//     <div>
-//       <button onClick={() => navigate("/aboutMe")}>About Me</button>
-//       <button onClick={() => navigate("/portfolio")}>Portfolio</button>
-//       <button onClick={() => navigate("/contact")}>Contact</button>
-//       <button onClick={() => navigate("/resume")}>Resume</button>
-//     </div>
+//       <ul>
+//         <li>About Me</li>
+//         <li>Portfolio</li>
+//         <li>Contact</li>
+//         <li>Resume</li>
+//       </ul>
 //   </div>
 //   );
 // }
-
-const Header = () => {
-  return (
-  <div>
-    <h1>
-      Liam Rayback
-    </h1>
-      <ul>
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
-      </ul>
-  </div>
-  );
-}
 
 export default Header; 
