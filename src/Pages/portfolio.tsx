b5a32c1dec5ef0fbc95a68e6626949a6ce45045e
Liam Rayback <liam@macbook.local>
2024-12-13 09:21:31 -0700

    started adding sections to pages

--- src/Pages/portfolio.tsx
+++ src/Pages/portfolio.tsx
@@ -1,7 +1,18 @@
 const Portfolio = () => {
     return (
         <div>
-            <h3>Portfolio</h3>
+            <div>
+                <h3>Portfolio</h3>
+            </div>
+            <div>
+                <h4>Vinyl Vault</h4>
+            </div>
+            <div>
+                <h4>Dead By Daylight</h4>
+            </div>
+            <div>
+                <h4>Mark Warner Digital Memorial</h4>
+            </div>
         </div>
     );
 }
--- src/Pages/resume.tsx
+++ src/Pages/resume.tsx
@@ -1,7 +1,15 @@
 const Resume = () => {
     return (
         <div>
-            <h3>Resume</h3>
+            <div>
+                <h3>Resume</h3>
+            </div>
+            <div>
+                <h4>Technical skills</h4>
+            </div>
+            <div>
+                <h4>Application skills</h4>
+            </div>
         </div>
     );
 }
