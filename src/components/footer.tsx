d7f54766d794ae570c27c37b245034c35fcdd1fe
Liam Rayback <liam@macbook.local>
2025-01-07 18:51:12 -0700

    added styling to add p tag below other items in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,32 @@
 
 const Footer = () => {
     return (
-        <footer className = "footer">
-            <a className = "footer-logo" 
-            onClick={() => window.open('https://github.com/KatanaFinKoi', '_blank')}
-            >
-                <img             
-                src = "/github-logo-6532.png" 
-                alt = "github logo"
-                >
-                </img>
-            </a>
-            
-            <a className = "footer-logo" 
-            onClick={() => window.open('https://www.linkedin.com/in/liam-rayback', '_blank')}
-            >
-                <img 
-                src = "/icons8-linkedin-192.png" 
-                alt = "linkedin logo"
-                >
-                </img>
-            </a>
+        <footer>
+            <div className = "footer">
+                <div className = "footer-logo-container">
+                    <a className = "footer-logo" 
+                    onClick={() => window.open('https://github.com/KatanaFinKoi', '_blank')}
+                    >
+                        <img             
+                        src = "/github-logo-6532.png" 
+                        alt = "github logo"
+                        >
+                        </img>
+                    </a>
+                    
+                    <a className = "footer-logo" 
+                    onClick={() => window.open('https://www.linkedin.com/in/liam-rayback', '_blank')}
+                    >
+                        <img 
+                        src = "/icons8-linkedin-192.png" 
+                        alt = "linkedin logo"
+                        >
+                        </img>
+                    </a>
+                </div>
+                <p className="footer-text">This portfolio is rendered using React!</p>
+            </div>
+
         </footer>
     );
 }
